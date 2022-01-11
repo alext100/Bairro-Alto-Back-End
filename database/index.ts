81c1c4f2190a4ba8f6b734bf1f9fbe1dd3e4aac2
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-01-11 17:10:12 +0100

    index.ts with initializeMongo() created

---
+++ database/index.ts
@@ -0,0 +1,33 @@
+import getDebug from "debug";
+import chalk from "chalk";
+import mongoose from "mongoose";
+import { ConnectionStringType, TransformTypes } from "../utils/types";
+
+const debug = getDebug("bairro:database");
+debug.enabled = true;
+
+const initializeMongo = (connectionString: ConnectionStringType) =>
+  new Promise<void>((resolve, reject) => {
+    mongoose.set("toJSON", {
+      virtuals: true,
+      transform: (doc: TransformTypes, ret: TransformTypes) => {
+        // eslint-disable-next-line no-underscore-dangle
+        delete ret._id;
+        // eslint-disable-next-line no-underscore-dangle
+        delete ret.__v;
+      },
+    });
+    mongoose.set("debug", true);
+    mongoose.connect(connectionString, (error) => {
+      if (error) {
+        debug(chalk.red("Failed connection with the database"));
+        debug(chalk.red(error.message));
+        reject();
+        return;
+      }
+      debug(chalk.greenBright(`Connected with the database `));
+      resolve();
+    });
+  });
+
+export default initializeMongo;
