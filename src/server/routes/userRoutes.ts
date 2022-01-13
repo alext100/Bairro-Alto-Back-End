d318ded98fccf9e8f37cac72d46a88dc35bbf69e
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-01-13 01:45:16 +0100

    To userRoutes userLogin and getAll pathes added

--- src/server/routes/userRoutes.ts
+++ src/server/routes/userRoutes.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import createUser from "../controllers/createUser.js";
+import loginUser from "../controllers/loginUser.js";
+import { getUsers } from "../controllers/userController.js";
+import paths from "../paths/paths.js";
 
 const router = express.Router();
 
-router.post("/register", createUser);
+router.post(paths.userRegister, createUser);
+router.post(paths.userLogin, loginUser);
+router.get(paths.getAll, getUsers);
 
 export default router;
