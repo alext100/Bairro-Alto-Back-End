d4dc1e76fbfa8018f80461ad8e66b68de28ccd7d
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-01-13 02:04:34 +0100

    To userController getOneUserById was added

--- src/server/controllers/userController.ts
+++ src/server/controllers/userController.ts
@@ -1,9 +1,31 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import User from "../../database/models/user.js";
+import { ErrorType } from "../../utils/types.js";
 
 const getUsers = async (req: Request, res: Response) => {
   const users = await User.find();
   res.json(users);
 };
 
-export { getUsers };
+const getOneUserById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+  try {
+    const searchedUsers = await User.findById(id);
+    if (searchedUsers) {
+      res.json(searchedUsers);
+    } else {
+      const error: ErrorType = new Error("User not found");
+      error.code = 404;
+      next(error);
+    }
+  } catch (error) {
+    (error as ErrorType).code = 400;
+    next(error);
+  }
+};
+
+export { getUsers, getOneUserById };
