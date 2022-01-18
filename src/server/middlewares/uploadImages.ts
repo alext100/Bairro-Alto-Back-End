d3b7fdba88faf3a2f440231e6ac011e04b72e3ef
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-01-18 02:30:16 +0100

    Function uploadImages with multer created

---
+++ src/server/middlewares/uploadImages.ts
@@ -0,0 +1,23 @@
+import multer from "multer";
+import path from "path";
+
+const uploadImages = multer({
+  storage: multer.diskStorage({
+    destination: "images",
+
+    filename: (req, file, callback) => {
+      const oldFilename = file.originalname;
+      const oldFilenameExtension = path.extname(oldFilename);
+      const oldFilenameWithoutExtension = oldFilename.replace(
+        oldFilenameExtension,
+        ""
+      );
+
+      const newFilename = `${oldFilenameWithoutExtension}-${Date.now()}${oldFilenameExtension}`;
+      callback(null, newFilename);
+    },
+  }),
+  limits: { fileSize: 3e6 },
+});
+
+export default uploadImages;
