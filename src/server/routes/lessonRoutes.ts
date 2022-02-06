55fb4171d3fbfe8ecfe6a38cd43470a794feefc5
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-02-07 00:15:07 +0100

    New route 'lesson/add' to lessonRoutes added

--- src/server/routes/lessonRoutes.ts
+++ src/server/routes/lessonRoutes.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { addLesson } from "../controllers/lessonControllers.js";
 import auth from "../middlewares/auth.js";
 import firebase from "../middlewares/firebase.js";
 import uploadImages from "../middlewares/uploadImages.js";
@@ -7,5 +8,6 @@ import paths from "../paths/paths.js";
 const router = express.Router();
 
 router.post(paths.uploadMedia, auth, uploadImages.array("images"), firebase);
+router.post(paths.create, auth, addLesson);
 
 export default router;
