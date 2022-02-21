f92b9f32d2e4ab30a0f8dad9ec018e98d1f3d23e
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-02-21 21:24:45 +0100

    Path 'deleteWebContent' to webContentRoutes added, auth added

--- src/server/routes/webContentRoutes.ts
+++ src/server/routes/webContentRoutes.ts
@@ -1,14 +1,18 @@
 import express from "express";
 import {
+  deletePost,
   getWebContent,
   updateWebContent,
 } from "../controllers/webContentControllers.js";
+import auth from "../middlewares/auth.js";
 import paths from "../paths/paths.js";
 
 const router = express.Router();
 
-router.get(paths.getWebContent, getWebContent);
+router.get(paths.getWebContent, auth, getWebContent);
 
-router.put(paths.updateWebContent, updateWebContent);
+router.put(paths.updateWebContent, auth, updateWebContent);
+
+router.delete(paths.deleteWebContent, auth, deletePost);
 
 export default router;
