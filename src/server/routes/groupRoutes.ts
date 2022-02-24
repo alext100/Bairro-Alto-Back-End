bf992a450f9b5bf0fcc8eed216af59b85df558f2
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-02-24 15:54:44 +0100

    Middlewares auth, teacherAuth, adminAuth to groupRoutes added

--- src/server/routes/groupRoutes.ts
+++ src/server/routes/groupRoutes.ts
@@ -10,26 +10,39 @@ import {
   deleteLessonFromGroup,
   toggleLessonInGroup,
 } from "../controllers/groupsController.js";
+import adminAuth from "../middlewares/adminAuth.js";
+import auth from "../middlewares/auth.js";
+import teacherAuth from "../middlewares/teacherAuth.js";
 import paths from "../paths/paths.js";
 
 const router = express.Router();
 
-router.get(paths.getAll, getAllGroups);
+router.get(paths.getAll, auth, teacherAuth, getAllGroups);
 
-router.post(paths.create, createGroup);
+router.post(paths.create, auth, teacherAuth, createGroup);
 
 router.get(paths.getById, getOneGroupById);
 
-router.put(paths.updateGroupById, updateGroupById);
+router.put(paths.updateGroupById, auth, teacherAuth, updateGroupById);
 
-router.patch(paths.addGroupToAnyUser, addMemberToGroup);
+router.patch(paths.addGroupToAnyUser, auth, teacherAuth, addMemberToGroup);
 
-router.patch(paths.deleteMemberFromGroup, deleteMemberFromGroup);
+router.patch(
+  paths.deleteMemberFromGroup,
+  auth,
+  teacherAuth,
+  deleteMemberFromGroup
+);
 
-router.patch(paths.deleteLessonFromGroup, deleteLessonFromGroup);
+router.patch(
+  paths.deleteLessonFromGroup,
+  auth,
+  teacherAuth,
+  deleteLessonFromGroup
+);
 
-router.patch(paths.addLessonToGroup, toggleLessonInGroup);
+router.patch(paths.addLessonToGroup, auth, teacherAuth, toggleLessonInGroup);
 
-router.delete(paths.delete, deleteGroup);
+router.delete(paths.delete, auth, adminAuth, deleteGroup);
 
 export default router;
