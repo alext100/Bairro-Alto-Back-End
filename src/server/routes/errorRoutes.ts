7ad8c818ea425c0bd48e13a1b9fc051114add431
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-02-24 15:55:18 +0100

    Middlewares auth, teacherAuth to errorRoutes added

--- src/server/routes/errorRoutes.ts
+++ src/server/routes/errorRoutes.ts
@@ -5,15 +5,22 @@ import {
   getAllGroupErrors,
   updateGroupError,
 } from "../controllers/errorsController.js";
+import auth from "../middlewares/auth.js";
+import teacherAuth from "../middlewares/teacherAuth.js";
 import paths from "../paths/paths.js";
 
 const router = express.Router();
 
 router.get(paths.getAllGroupErrors, getAllGroupErrors);
 
-router.patch(paths.addErrorToGroup, addErrorToGroup);
-router.patch(paths.deleteErrorFromGroup, deleteErrorFromGroup);
+router.patch(paths.addErrorToGroup, auth, teacherAuth, addErrorToGroup);
+router.patch(
+  paths.deleteErrorFromGroup,
+  auth,
+  teacherAuth,
+  deleteErrorFromGroup
+);
 
-router.put(paths.updateGroupError, updateGroupError);
+router.put(paths.updateGroupError, auth, teacherAuth, updateGroupError);
 
 export default router;
