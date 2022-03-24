a8bf31e1cf69656f7be66003dc408ab6ec3e5bb4
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-03-24 19:57:10 +0100

    Login controller refactored for check user.status

--- src/server/controllers/loginUser.ts
+++ src/server/controllers/loginUser.ts
@@ -5,6 +5,7 @@ import getDebug from "debug";
 import chalk from "chalk";
 import User from "../../database/models/user.js";
 import { ErrorType } from "../../utils/types.js";
+import sendConfirmationEmail from "./sendConfirmationEmail.js";
 
 const debug = getDebug("bairro:login");
 debug.enabled = true;
@@ -18,13 +19,20 @@ const loginUser = async (req: Request, res: Response, next: NextFunction) => {
     error.code = 401;
     next(error);
   } else {
-    debug(chalk.green("An user is logged in"));
+    debug(chalk.green("An user is logging in"));
     const rightPassword = await bcrypt.compare(password, user.password);
     if (!rightPassword) {
+      debug(chalk.red("Wrong password"));
       const error: ErrorType = new Error("Wrong credentials");
       error.code = 401;
       next(error);
-    } else {
+    } else if (user.status !== "Active") {
+      debug(chalk.red("Not active"));
+      const error: ErrorType = new Error("Invalid token specified");
+      error.code = 401;
+      return res.send(error);
+    } else if (user.status === "Active") {
+      debug(chalk.red("Is active, logged in"));
       const token = jwt.sign({ email, id: user.id }, secretHash, {
         expiresIn: 72 * 60 * 60,
       });
@@ -33,4 +41,4 @@ const loginUser = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export default loginUser;
+export { loginUser, verifyUser, sendConfirmEmailOneMoreTime };
