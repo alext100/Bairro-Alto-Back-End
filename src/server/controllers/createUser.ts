ccfb4498ac0234bb72cf429740371a02e1535a11
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-03-24 19:55:02 +0100

    To createUser controller sendConfirmationEmail() added

--- src/server/controllers/createUser.ts
+++ src/server/controllers/createUser.ts
@@ -1,10 +1,15 @@
 import { Request, Response, NextFunction } from "express";
-
+import dotenv from "dotenv";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import User from "../../database/models/user.js";
 import { ErrorType } from "../../utils/types.js";
+import sendConfirmationEmail from "./sendConfirmationEmail.js";
+
+dotenv.config();
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const secretHash: string = process.env.GOOGLE_MAIL_SECRET_HASH as string;
   const {
     email,
     password,
@@ -17,7 +22,10 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     groups,
     studentErrors,
     homeworkToCheck,
+    status,
   } = req.body;
+
+  const token = jwt.sign({ email: req.body.email }, secretHash);
   try {
     const user = await User.create({
       email,
@@ -31,8 +39,11 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
       studentErrors,
       homeworkToCheck,
       password: await bcrypt.hash(password, 10),
+      status,
+      confirmationCode: token,
     });
     res.status(201).json(user);
+    sendConfirmationEmail(user.firstName, user.email, user.confirmationCode);
   } catch {
     const error: ErrorType = new Error("Bad credentials provided");
     error.code = 400;
